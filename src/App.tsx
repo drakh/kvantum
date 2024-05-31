c8f7b05f6272d21d6f91e6fa44628c7dc0c137f6
Lubomir Panak <lubopanak@gmail.com>
2024-05-31 12:06:57 +0200

    initial preparation of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { type FC } from 'react';
+// @ts-ignore
+import { ARView, ARAnchor } from "react-three-mind";
 
 const App: FC = () => {
   return (
     <main>
       <header>Hello world</header>
+        <ARView>
+            <ARAnchor>
+
+            </ARAnchor>
+        </ARView>
     </main>
   );
 };
