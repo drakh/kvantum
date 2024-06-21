d453ea0b318f0e899f03faa72f6291e58be4f4af
Lubomir Panak <lubopanak@gmail.com>
2024-06-21 14:38:12 +0200

    gltf animation test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { type FC, useEffect } from 'react';
-import { useGLTF, useAnimations } from '@react-three/drei';
+import {
+  useGLTF,
+  useAnimations,
+  /*Mask,*/ useMask,
+  Environment,
+  MeshPortalMaterial,
+} from '@react-three/drei';
 import { ARView, ARAnchor } from './react-three-mind/AR';
 // import { LoopOnce } from 'three';
 
@@ -7,18 +13,63 @@ const Model: FC = () => {
   const { scene, animations } = useGLTF('cube.gltf');
   const { clips, mixer } = useAnimations(animations, scene);
 
+  console.info({ scene });
+
   useEffect(() => {
-    const [clip] = clips;
+    scene.children.forEach((child) => {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    });
     console.info({ clips });
+    clips.forEach((clip) => {
+      mixer.clipAction(clip).play();
+    });
     // mixer.clipAction(clip).loop = LoopOnce;
-    mixer.clipAction(clip).play();
-    clip.duration;
-    mixer.clipAction(clip).halt(clip.duration / 1.2);
+    // clip.duration;
+    // mixer.clipAction(clip).halt(clip.duration / 1.2);
   }, [clips, mixer]);
 
-  console.info({ scene, animations, clips, mixer });
+  scene.castShadow = true;
+  scene.receiveShadow = true;
 
-  return <primitive object={scene} />;
+  const stencil = useMask(1);
+
+  console.info({ stencil });
+
+  return (
+    <>
+      <mesh>
+        <primitive object={scene} />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <planeGeometry args={[10, 10]} />
+        <MeshPortalMaterial>
+          <ambientLight intensity={2} />
+          <hemisphereLight intensity={1} groundColor="red" />
+          <Environment preset="city" />
+          <mesh castShadow receiveShadow>
+            <primitive object={scene} />
+          </mesh>
+        </MeshPortalMaterial>
+      </mesh>
+      {/*<Mask id={1} position={[0, 0, 0.95]}>*/}
+      {/*  <planeGeometry />*/}
+      {/*</Mask>*/}
+      {/*  <mesh>*/}
+      {/*    <planeGeometry />*/}
+      {/*  </mesh>*/}
+
+      {/*<Mask id={1}>*/}
+      {/*  <planeGeometry />*/}
+      {/*  <meshBasicMaterial />*/}
+      {/*</Mask>*/}
+      {/*<mesh>*/}
+      {/*  <torusKnotGeometry />*/}
+      {/*  <meshStandardMaterial {...stencil} />*/}
+      {/*  <primitive object={scene} />*/}
+      {/*</mesh>*/}
+    </>
+  );
 };
 
 const App: FC = () => {
@@ -26,9 +77,6 @@ const App: FC = () => {
     <main>
       <header>Hello world</header>
       <ARView>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
         <ARAnchor></ARAnchor>
         <Model />
       </ARView>
